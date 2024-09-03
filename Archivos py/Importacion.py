5f90bd28f22d77125072c9f0caaddf0f7ce56fb7
Juan-PC <jillanes.va@gmail.com>
2024-09-03 00:53:47 -0400

    Rehice el diccionario y dejé creada la matriz de volumen de producción

--- Archivos py/Importacion.py
+++ Archivos py/Importacion.py
@@ -1,5 +1,7 @@
 from pandas import read_csv
+
 #nombre_archivo = r'wrd_04_all-data.csv'
+#columnas = ['designer_country', 'award_category', 'award_period', 'award_score']
 
 def carga(nombre_archivo: str, columnas_importantes: list):
     '''Función que toma el nombre del archivo (que DEBE estar en la carpeta /Datos) en formato csv y e immporta las columnas importantes limpiando los NaNs'''
--- Archivos py/Tratamiento.py
+++ Archivos py/Tratamiento.py
@@ -1,11 +1,37 @@
 from Importacion import carga
+import numpy as np
 
-def set_of_columns(data):
+def domain_of_data(data):
+    '''TO DO'''
     lista_de_elementos = []
-    todas_las_columnas = list(data)
-    for index in todas_las_columnas:
-        lista_de_elementos.append(list(set(data[index])))
-    return todas_las_columnas, lista_de_elementos
+    for index in list(data):
+        lista_de_elementos.append(sorted(list(set(data[index]))))
+    return lista_de_elementos, [len(lista) for lista in lista_de_elementos]
+
+#¿Debería unir a una sola función dictionaries y domain_of_data?
+
+def dictionaries(data):
+    '''TO DO'''
+    lista_de_elementos, cantidad_de_elementos = domain_of_data(data)
+    diccionarios = []
+    for index in range(len(cantidad_de_elementos)):
+        diccionarios.append(dict( [ (lista_de_elementos[index][n], n) for n in range(cantidad_de_elementos[index])] ))
+    return diccionarios
+
+def distributividad(d, l):
+    pass
+
+def X_matrix(data):
+    _, cantidad_elementos = domain_of_data(data)
+    diccionarios = dictionaries(data)
+    X_cpt = np.zeros(cantidad_elementos[0:3])
+    Valores = data.values
+
+    for N in range(len(Valores.shape[0])):
+        index = ()
+        X_cpt[  ]
+    return X_cpt
+
 
 
 
